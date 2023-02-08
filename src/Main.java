18ba1a70aa2ee7bd77e8bce2bee8721eb5a62a33
x3imal <aasurov@gmail.com>
2023-02-08 13:58:40 +0300

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -5,43 +5,4 @@ public class Main {
 
         }
 
-        public static void task1 () {
-            byte cat = -29;
-            short mouse = 366;
-            int dog = 56555;
-            long fly = 9996651;
-            float apple = 1.8F;
-            double orange = 6.556;
-            System.out.println("Значние переменной cat c типом byte равно " + cat);
-            System.out.println("Значние переменной mouse c типом short равно " + mouse);
-            System.out.println("Значние переменной dog c типом int равно " + dog);
-            System.out.println("Значние переменной fly c типом long равно " + fly);
-            System.out.println("Значние переменной apple c типом float равно " + apple);
-            System.out.println("Значние переменной orange c типом double равно " + orange);
-        }
-
-        public static void task2 () {
-            float potato = 27.12F;
-            System.out.println(potato);
-
-            long bottle = 987678965549L;
-            System.out.println(bottle);
-
-            double tomato = 2.786;
-            System.out.println(tomato);
-
-            short pan = 569;
-            System.out.println(pan);
-
-            short temp = -159;
-            System.out.println(temp);
-
-            short money = 27897;
-            System.out.println(money);
-
-            byte all = 67;
-            System.out.println(all);
-    }
-
-
 }
