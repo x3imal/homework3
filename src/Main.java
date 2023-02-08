8423cee25520fa7b4ac2216d409a13a7726041d3
x3imal <aasurov@gmail.com>
2023-02-08 13:50:09 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+
+        }
+
+        public static void task1 () {
+            byte cat = -29;
+            short mouse = 366;
+            int dog = 56555;
+            long fly = 9996651;
+            float apple = 1.8F;
+            double orange = 6.556;
+            System.out.println("Значние переменной cat c типом byte равно " + cat);
+            System.out.println("Значние переменной mouse c типом short равно " + mouse);
+            System.out.println("Значние переменной dog c типом int равно " + dog);
+            System.out.println("Значние переменной fly c типом long равно " + fly);
+            System.out.println("Значние переменной apple c типом float равно " + apple);
+            System.out.println("Значние переменной orange c типом double равно " + orange);
+        }
+
+
+        }
+}
